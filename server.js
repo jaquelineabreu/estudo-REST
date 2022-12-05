046817181dc6b7914f64a32d683464d125e240b6
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2022-12-05 00:30:06 -0300

    Projeto1 - Estudo de REST finalizado

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const data = require("./data.json");
+
+//express usa a notação json
+app.use(express.json());
+
+app.listen(3000, function(){
+    console.log("Server is running");
+});
+
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+app.get("/clients/:id", function(req, res){
+
+    const{ id } = req.params;
+    const client = data.find(cli => cli.id = id);
+
+    if(!client) return res.status(204).json();
+
+    res.json(client);
+});
+app.post("/clients", function(req, res){
+
+    const {name, email} = req.body;
+
+    //logica para salvar
+
+    res.json({name, email});
+
+});
+app.put("/clients/:id", function(req, res){
+    const{ id } = req.params;
+    const client = data.find(cli => cli.id = id);
+
+    if(!client) return res.status(204).json();
+
+    const { name } = req.body;
+
+    client.name = name;
+
+    res.json(client);
+});
+app.delete("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const clientFiltered = data.filter(client => client.id != id);
+
+    res.json(clientFiltered);
+
+});
